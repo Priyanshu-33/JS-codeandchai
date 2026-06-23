c6ca70177d1933a45e2c1d05fe3de45e1139a966
Priyanshu-33 <priyanshu0632@gmail.com>
2026-06-23 07:54:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+4)  // we are using nodejs not browser
+
+console.log(3 +3)   
+console.log("hemanu")
+
+let name="piyush"  //string
+let age=17  //number
+let isLoggedIn=false  //boolean
+
+//null =>standalone value
+// undefined => value assigned nhi h
+//symbol => unique 
+
+
+
+//object
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
+
+
+
+
