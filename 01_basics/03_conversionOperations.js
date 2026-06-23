00d5cc72532c9fefb5e7c8c2fdb2ea631c48e34f
Priyanshu-33 <priyanshu0632@gmail.com>
2026-06-23 09:05:52 +0000

    datatypes conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score=null
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber =Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//NaN is Not an Number
+//score =null  tab type object print 0 
+// score= undefined tab type undefined print NaN
+
+//"33" =>33
+//"33abc"=>NaN
+//true =>1;false=>0
+
+let isLoggedIn="p"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// "" =>false
+// "pgh"=>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
