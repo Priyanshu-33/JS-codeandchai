010ba5de48f31900ed7b9033e30bf20ff0c5b70f
Priyanshu-33 <priyanshu0632@gmail.com>
2026-06-24 04:52:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);      
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);    //string number me change ho skta h
+// console.log("02" > 1);   //string number me change ho skta h
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// null < 0   // false
+// null <= 0  // true
+// null > 0   // false
+// null >= 0  // true
+// null == 0  // false
+// > aur >= comparison me null → 0
+// == comparison me null sirf undefined ke equal hota hai
+
+console.log(undefined == 0);  //false
+console.log(undefined > 0);   //false
+console.log(undefined < 0);  //false
+
+
+console.log("2" === 2);  //=== type aur value dono check krta h
